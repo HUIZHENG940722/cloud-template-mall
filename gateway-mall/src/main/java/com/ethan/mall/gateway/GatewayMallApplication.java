19296f2350bd23091c5f0d98a37116f8720bb878
zhenghui <15270855286@163.com>
2022-03-06 12:42:23 +0800

    电商模版微服务框架初步提交

---
+++ gateway-mall/src/main/java/com/ethan/mall/gateway/GatewayMallApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMallApplication.class, args);
+    }
+
+}
