8d292e02b6151b2bc2b3189241915150c7e87b0e
zhenghui <15270855286@163.com>
2022-03-06 21:47:22 +0800

    网关配置oauth2服务模块路由

---
+++ oauth2-mall/src/main/java/com/ethan/mall/oauth2/Oauth2MallApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.mall.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2MallApplication.class, args);
+    }
+
+}
