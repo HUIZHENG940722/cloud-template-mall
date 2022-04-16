b5d9c85f7452d75fd43ab7b14088cf5d9d357a36
zhenghui <15270855286@163.com>
2022-04-16 19:56:06 +0800

    增加认证提示信息

--- oauth2-mall/src/main/java/com/ethan/mall/oauth2/service/impl/AuthorizationServiceImpl.java
+++ oauth2-mall/src/main/java/com/ethan/mall/oauth2/service/impl/AuthorizationServiceImpl.java
@@ -3,6 +3,10 @@ package com.ethan.mall.oauth2.service.impl;
 import com.ethan.mall.common.domain.AuthenticationUser;
 import com.ethan.mall.oauth2.domain.AuthorizationUser;
 import com.ethan.mall.oauth2.service.feign.AdminService;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,7 +28,19 @@ public class AuthorizationServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         AuthenticationUser authenticationUser = adminService.loadUserByUsername(username);
+        if (authenticationUser == null) {
+            throw new UsernameNotFoundException("用户名或密码错误!");
+        }
         AuthorizationUser authorizationUser = new AuthorizationUser(authenticationUser);
+        if (!authorizationUser.isEnabled()) {
+            throw new DisabledException("该账户已被禁用，请联系管理员!");
+        } else if (!authorizationUser.isAccountNonLocked()) {
+            throw new LockedException("该账号已被锁定，请联系管理员!");
+        } else if (!authorizationUser.isAccountNonExpired()) {
+            throw new AccountExpiredException("该账号已过期，请联系管理员!");
+        } else if (!authorizationUser.isCredentialsNonExpired()) {
+            throw new CredentialsExpiredException("该账户的登录凭证已过期，请重新登录!");
+        }
         return authorizationUser;
     }
 }
